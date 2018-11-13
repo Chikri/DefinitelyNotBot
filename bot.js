4d32d05e2b447daa94dd6a13d9d81448577a3b58
Chikri <37224384+Chikri@users.noreply.github.com>
2018-11-13 18:48:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,15 @@ client.on('message', msg => {
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
 
+client.on('message', msg => {
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    const args = msg.content.split(' ').slice(1).join(' ');
+    if (command === 'help') return msg.author.send('Note: All commands need to have prefix | guide = shows a guide on how to use the bot | invite = shows invite for the bot | info = shows info about the bot');
+    else if (command === 'info') return msg.channel.send(process.env.INFO);
+});
+
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
