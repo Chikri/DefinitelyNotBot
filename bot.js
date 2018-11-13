fec390a21850d41cfd24468b07892f8d4bc38f97
Chikri <37224384+Chikri@users.noreply.github.com>
2018-11-13 18:58:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,28 @@ client.on('message', msg => {
     if (command === 'invite') return msg.channel.send(process.env.INVITE);
     if (command === 'help') return msg.author.send('Note: All commands need to have prefix | guide = shows a guide on how to use the bot | invite = shows invite for the bot | info = shows info about the bot');
     if (command === 'info') return msg.channel.send(process.env.INFO);
+    if (command === 'ban') 
+    if (!msg.member.permissions.has("BAN_MEMBERS")) return msg.channel.send(":warning: Insufficient Permissions").catch(console.error);
+    if (!msg.guild.member(client.user).permissions.has("BAN_MEMBERS")) return msg.channel.send(":warning: Bot has insufficient permissions").catch(console.error);
+
+  if (msg.mentions.users.size === 0) return msg.channel.send("No user provided")
+
+  if (msg.author.id === msg.mentions.members.first().user.id) return msg.channel.send("You can't ban yourself").catch(console.error);
+
+  if (client.user.id === msg.mentions.users.first().id) return msg.channel.send(`Don't try to ban me, ${msg.author.username}`).catch(console.error);
+
+  userToBan.ban()
+    var user = msg.mentions.users.first()
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:hammer: User Banned: ${user.tag} (${user.id})`)
+    .setColor(0xd11212)
+    .addField("Responsible Moderator:", `${msg.author.tag} (${msg.author.id})\n\nReason: ${reason}`)
+    .setTimestamp(new Date(msg.createdTimestamp))
+
+    msg.guild.channels.find("name", "mod-logs").send({embed});
+  }
+
 });
-
-
 
 
 
