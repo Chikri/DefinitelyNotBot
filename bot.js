f5e7164849b358fabfc1f56bd7b8dd2a9b098b3d
Chikri <37224384+Chikri@users.noreply.github.com>
2018-11-13 19:14:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,20 @@ client.on('message', msg => {
     const args = msg.content.split(' ').slice(1).join(' ');
     if (command === 'guide') return msg.channel.send('pastebin.com');
     if (command === 'invite') return msg.channel.send(process.env.INVITE);
+    if (command === 'serverinfo')
+      const embed = new Discord.RichEmbed()
+
+    .setTitle(`${msg.guild.name}`)
+    .setColor(0x17bec6)
+    .addField(`Owner`, `${msg.guild.owner.user.tag} (${msg.guild.owner.id})`)
+    .addField(`Members`, `${msg.guild.memberCount}`)
+    .addField(`Region`, `${msg.guild.region}`)
+    .addField(`ID`, `${msg.guild.id}`)
+    .addField(`Channels`, `${msg.guild.channels.size}`)
+    .addField(`Created at`, `Created at date: WIP`)
+
+    msg.channel.send({embed});
+  }
     if (command === 'help') return msg.author.send('Note: All commands need to have prefix | guide = shows a guide on how to use the bot | invite = shows invite for the bot | info = shows info about the bot');
     if (command === 'info') return msg.channel.send(process.env.INFO);
     if (command === 'eval')
